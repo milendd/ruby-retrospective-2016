1d461294a4194a1309e87e581e46668be1f67aed
milendd <milen_d@abv.bg>
2017-01-12 15:26:11 +0200

    Implement #reshape in task 2 to pass the tests.

--- tasks/2/solution.rb
+++ tasks/2/solution.rb
@@ -10,7 +10,7 @@ class Hash
   @next_deep_element
   
   def fetch_deep_recursive(current_element, components)
-    if (current_element.is_a?Hash) || (current_element.is_a?Array)
+    if (current_element.is_a? Hash) || (current_element.is_a? Array)
       @next_deep_element = current_element
       fetch_deep components[1..-1].join('.')
     else
@@ -24,5 +24,26 @@ class Hash
     @next_deep_element ||= self
     current = get_next_element(@next_deep_element, components.first)
     fetch_deep_recursive(current, components)
+  end
+  
+  def reshape_recursive(shape, key)
+    if shape[key].is_a? String
+      fetch_deep(shape[key])
+    else
+      reshape(shape[key])
+    end
+  end
+  
+  def reshape(shape)
+    shape.each_key do |key|
+      shape[key] = reshape_recursive(shape, key)
+    end
+    shape
+  end
+end
+
+class Array
+  def reshape(shape)
+    each.map { |x| x.reshape(shape.dup) }
   end
 end
